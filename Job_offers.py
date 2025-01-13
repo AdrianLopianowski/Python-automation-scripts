200858ede0b177a0af3ce2741d3750d997f1787a
AdrLop <adrianlopianowski@gmail.com>
2025-01-13 23:52:20 +0100

    Introduced functionality for sending emails using Python's smtplib.

--- Job_offers.py
+++ Job_offers.py
@@ -1,9 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import os 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
 
 # File to store all offers in JSON format
 offers_file = "offers.json"
+load_dotenv()
+sender_email = os.getenv("EMAIL_ADDRESS")
+recipient_email = os.getenv("RECIPIENT_EMAIL")
+password = os.getenv("EMAIL_PASSWORD")
+smtp_server = os.getenv("SMTP_SERVER")
+port = int(os.getenv("SMTP_PORT"))
+print("Loaded configuration:")
+print(f"SMTP Server: {smtp_server}")
+print(f"Port: {port}")
+print(f"Sender Email: {sender_email}")
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+# Testowe dane
+subject = "Testowy e-mail"
+body = "Cześć! To jest testowy e-mail wysłany z Pythona."
+
+
+
+
+def send_email(recipient_email, subject, body):
+    try:
+        # Stwórz wiadomość e-mail
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = recipient_email
+        msg['Subject'] = subject
+
+        # Dodaj treść wiadomości
+        msg.attach(MIMEText(body, 'plain'))
+
+        # Połącz się z serwerem SMTP
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls()  # Szyfruj połączenie
+            server.login(sender_email, password)
+            server.send_message(msg)
+
+        print("Email wysłany pomyślnie!")
+    except Exception as e:
+        print(f"Wystąpił błąd podczas wysyłania e-maila: {e}")
 
 def load_offers():
     """Load all previously stored offers from a JSON file."""
@@ -55,3 +100,5 @@ if response.status_code == 200:
     # Save current offers to the same file
     save_offers(current_offers)
     print(f"All offers saved to {offers_file}.")
+    # Wywołaj funkcję
+send_email(recipient_email, subject, body)
